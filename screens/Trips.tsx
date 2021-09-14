0c872bf30d316d0008f187d68e26c7c7fde2e645
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-14 12:05:43 +0100

    start trips layout

--- screens/Trips.tsx
+++ screens/Trips.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet, Button } from 'react-native';
 
-const Trips = () => {
+const Trips = (props: any) => {
+  const nav = props.navigation;
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text>This is the Trips Screen!</Text>
+        <View style={styles.logo}> logo</View>
+        <Button
+          title='Check Country'
+          onPress={() => nav.navigate('IndividualCountry')}
+        ></Button>
+        <View style={styles.myTripsContainer}>
+          <Text style={styles.myTripsTitle}>My Trips</Text>
+          <View style={styles.singleTripContainer}>
+            <Text style={styles.info}>Trip 1</Text>
+            <Text style={styles.info}>info 1</Text>
+            <Text style={styles.info}>info 2</Text>
+            <Text style={styles.info}>info 3</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -16,8 +30,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  logo: {
+    flex: 1,
+    height: 20,
+    padding: 20,
+    margin: 10,
+    borderRadius: 80,
+    alignItems: 'stretch',
+    backgroundColor: '#5f9ea0'
+  },
+  myTripsContainer: {
+    backgroundColor: '#fff8dc',
+    flex: 2,
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+    margin: 25,
+    padding: 50
+  },
+  myTripsTitle: {
+    alignSelf: 'center',
+    margin: 5
+  },
+  singleTripContainer: {
+    backgroundColor: '#a9a9a9',
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+    margin: 5,
+    height: 80,
+    padding: 20
+  },
+  info: {
+    margin: 2,
+    alignSelf: 'center'
+  }
 });
 
 export default Trips;
