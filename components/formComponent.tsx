b019bbca9fcef8d52ef91ba8fa5b70598da4cdbc
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-15 12:58:36 +0100

    add form validation

--- components/formComponent.tsx
+++ components/formComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -9,14 +9,30 @@ import {
   Button
 } from 'react-native';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
+const userSchema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().min(4).max(10).required()
+  // can do a .matches(takes a regex for password rules)
+  // can do a .test(takes 3 args, name of func, message to user, function to test input)
+});
 const formComponent = () => {
+  const [user, setUser] = useState({
+    name: 'Bob',
+    email: 'bob@cat.com',
+    password: 'ghj'
+  });
+
   return (
     <View style={styles.formContainer}>
       <Formik
         initialValues={{ name: '', email: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values);
+        validationSchema={userSchema}
+        onSubmit={(values, actions) => {
+          setUser(values);
+          actions.resetForm();
         }}
       >
         {(props) => (
@@ -26,56 +42,95 @@ const formComponent = () => {
               placeholder='name'
               onChangeText={props.handleChange('name')}
               value={props.values.name}
+              onBlur={props.handleBlur('name')}
             />
+            <Text style={styles.errorText}>
+              {' '}
+              {props.touched.name && props.errors.name}{' '}
+            </Text>
 
             <TextInput
               style={styles.input}
               placeholder='email'
               onChangeText={props.handleChange('email')}
               value={props.values.email}
+              onBlur={props.handleBlur('email')}
             />
-
+            <Text style={styles.errorText}>
+              {' '}
+              {props.touched.email && props.errors.email}{' '}
+            </Text>
             <TextInput
               style={styles.input}
               placeholder='password'
               onChangeText={props.handleChange('password')}
               value={props.values.password}
+              onBlur={props.handleBlur('password')}
             />
+            <Text style={styles.errorText}>
+              {' '}
+              {props.touched.password && props.errors.password}{' '}
+            </Text>
 
-            <Button
-              title='submit'
-              color='red'
+            <TouchableOpacity
+              style={styles.btn}
               onPress={() => {
-                props.handleSubmit;
+                props.handleSubmit();
               }}
-            />
+            >
+              <Text style={styles.btnText}> Sign Up </Text>
+            </TouchableOpacity>
           </View>
         )}
       </Formik>
+      {console.log(user)}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   formContainer: {
-    height: 300,
+    alignSelf: 'center',
+    height: 350,
     width: 300,
     padding: 20,
-    margin: 50,
-    alignItems: 'stretch',
-    borderColor: 'red',
-    borderWidth: 1
+    margin: 30,
+    alignItems: 'center',
+    borderWidth: 3,
+    borderColor: '#ddd',
+    borderRadius: 6
   },
   input: {
     height: 30,
     alignItems: 'stretch',
     backgroundColor: 'white',
-    borderWidth: 3,
+    borderWidth: 2,
     borderColor: '#ddd',
     padding: 10,
     margin: 10,
     fontSize: 18,
     borderRadius: 6
+  },
+  errorText: {
+    color: 'crimson',
+    fontWeight: 'bold',
+    marginBottom: 3,
+    marginTop: 3,
+    textAlign: 'center'
+  },
+  btn: {
+    borderRadius: 8,
+    marginVertical: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    backgroundColor: '#cd5c5c'
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    textAlign: 'center'
   }
 });
 
