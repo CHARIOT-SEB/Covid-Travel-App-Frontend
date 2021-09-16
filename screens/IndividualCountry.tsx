35d91c1228a75d6a6a7cd036ff567b737ee5bbcf
Mahamud Arteh <artehrush@gmail.com>
2021-09-16 10:10:42 +0100

    Revert "Map"

--- screens/IndividualCountry.tsx
+++ screens/IndividualCountry.tsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Button, Alert } from "react-native";
-import MapView from "react-native-maps";
-import { SafeAreaView } from "react-native-safe-area-context";
-import * as Location from "expo-location";
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const IndividualCountry = (props: any) => {
-  // will figure out a way to display the user's location if enabled
-  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
-  //initial region currently set to france
-  const [region, setRegion] = useState({
-    initialRegion: {
-      latitude: 47.4256,
-      longitude: 2.6054,
-      latitudeDelta: 10,
-      longitudeDelta: 0.25,
-    },
-  });
-
   const nav = props.navigation;
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          region={region.initialRegion}
-          showsUserLocation={true}
-        />
-        <View style={styles.container}></View>
+        <View style={styles.logo}> logo</View>
+        <View style={styles.countryTitle}>
+          <Text>France</Text>
+        </View>
+        <View style={styles.heatMapContainer}>
+          <Text style={styles.heatMap}>Heat Map Contents</Text>
+        </View>
+        <View style={styles.countryInfoContainer}>
+          <Text style={styles.info}>info 1</Text>
+          <Text style={styles.info}>info 2</Text>
+          <Text style={styles.info}>info 3</Text>
+          <Text style={styles.info}>info 4</Text>
+        </View>
+        <Button
+          title='Add to trips'
+          onPress={() => nav.navigate('Trips')}
+        ></Button>
       </View>
     </SafeAreaView>
   );
@@ -34,18 +31,9 @@ const IndividualCountry = (props: any) => {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  map: {
-    alignSelf: "center",
-    width: 370,
-    height: 500,
-
-    alignItems: "center",
-    borderWidth: 3,
-    borderColor: "#ddd",
-    borderRadius: 6,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   logo: {
     flex: 1,
@@ -53,51 +41,50 @@ const styles = StyleSheet.create({
     padding: 20,
     margin: 10,
     borderRadius: 80,
-    alignItems: "stretch",
-    backgroundColor: "#5f9ea0",
+    alignItems: 'stretch',
+    backgroundColor: '#5f9ea0'
   },
   countryTitle: {
     height: 5,
     margin: 20,
     padding: 20,
     width: 70,
-    backgroundColor: "#ff7f50",
+    backgroundColor: '#ff7f50',
     borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   heatMapContainer: {
     flex: 3,
-    flexDirection: "row",
+    flexDirection: 'row',
     margin: 25,
     height: 20,
-    alignItems: "stretch",
-    alignSelf: "stretch",
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
     padding: 50,
-    backgroundColor: "#fff8dc",
-    justifyContent: "center",
+    backgroundColor: '#fff8dc',
+    justifyContent: 'center'
   },
   heatMap: {
-    flexDirection: "row",
+    flexDirection: 'row',
     margin: 10,
-    color: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
+    color: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   countryInfoContainer: {
-    backgroundColor: "#fff8dc",
+    backgroundColor: '#fff8dc',
     flex: 4,
-    flexDirection: "column",
-    alignSelf: "stretch",
+    flexDirection: 'column',
+    alignSelf: 'stretch',
     margin: 25,
     height: 20,
-    padding: 50,
+    padding: 50
   },
   info: {
     margin: 2,
-    alignSelf: "center",
-  },
-  text: {},
+    alignSelf: 'center'
+  }
 });
 
 export default IndividualCountry;
