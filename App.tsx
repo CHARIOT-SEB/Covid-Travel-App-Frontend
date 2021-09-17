6a8bb73f00e0cb8985c2f6f525fbf44d63c3afc4
Rob Crowther <robcrowther@hotmail.com>
2021-09-17 17:29:51 +0100

    continue to work on the country picker functionality

--- App.tsx
+++ App.tsx
@@ -9,32 +9,32 @@ import MyStack from './navigation/MyStack';
 import LandingPage from './screens/LandingPage';
 
 const App = () => {
-	return (
-		<SafeAreaProvider>
-			<Modal visible={false}>
-				<LandingPage />
-			</Modal>
-			<View>
-				<MyStack />
-			</View>
-			<View style={styles.container}>
-				<MyTabs />
-			</View>
-			<StatusBar style='auto' />
-		</SafeAreaProvider>
-	);
+  return (
+    <SafeAreaProvider>
+      <Modal visible={false}>
+        <LandingPage />
+      </Modal>
+      <View>
+        <MyStack />
+      </View>
+      <View style={styles.container}>
+        <MyTabs />
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-	tabs: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'space-evenly',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  tabs: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  }
 });
 
 export default App;
