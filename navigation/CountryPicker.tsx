2d9e2c052ae6bdc682bf1dc8d3d8f9b3829c4ee5
Mahamud Arteh <artehrush@gmail.com>
2021-09-18 09:37:46 +0100

    geoLocations rendering correctly

--- navigation/CountryPicker.tsx
+++ navigation/CountryPicker.tsx
@@ -8,60 +8,60 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 function CountryPicker(props: any) {
-  const nav: any = props.nav;
-  const [country, setCountry] = useState('');
-  const [countries, setCountries] = useState([
-    'Austria',
-    'Belgium',
-    'Bulgaria',
-    'Croatia',
-    'Cyprus',
-    'czechRepublic',
-    'Denmark',
-    'Estonia',
-    'Finland',
-    'France'
-  ]);
+	const nav: any = props.nav;
+	const [country, setCountry] = useState('');
+	const [countries, setCountries] = useState([
+		'Austria',
+		'Belgium',
+		'Bulgaria',
+		'Croatia',
+		'Cyprus',
+		'czechRepublic',
+		'Denmark',
+		'Estonia',
+		'Finland',
+		'France',
+	]);
 
-  console.log(country);
+	console.log(country);
 
-  return (
-    <View style={styles.screen}>
-      <Text style={styles.text}>See COVID Travel information by country</Text>
-      <Picker
-        selectedValue={country}
-        onValueChange={(value, index) => setCountry(value)}
-        mode="dropdown" // Android only
-        style={styles.picker}
-      >
-        <Picker.Item label="Please choose a country" value="Unknown" />
-        {countries.map((country) => {
-          return <Picker.Item key={country} label={country} value={country} />;
-        })}
-      </Picker>
-      <Button title="Go" onPress={() => nav.navigate('Country', country)} />
-    </View>
-  );
+	return (
+		<View style={styles.screen}>
+			<Text style={styles.text}>See COVID Travel information by country</Text>
+			<Picker
+				selectedValue={country}
+				onValueChange={(value, index) => setCountry(value)}
+				mode='dropdown' // Android only
+				style={styles.picker}
+			>
+				<Picker.Item label='Please choose a country' value='Unknown' />
+				{countries.map(country => {
+					return <Picker.Item key={country} label={country} value={country} />;
+				})}
+			</Picker>
+			<Button title='Go' onPress={() => nav.navigate('Country', country)} />
+		</View>
+	);
 }
 
 export default CountryPicker;
 
 // Some default styles
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightblue'
-  },
-  text: {
-    fontSize: 24
-  },
-  picker: {
-    marginVertical: 30,
-    width: 300,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#666'
-  }
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'lightblue',
+	},
+	text: {
+		fontSize: 24,
+	},
+	picker: {
+		marginVertical: 30,
+		width: 300,
+		padding: 10,
+		borderWidth: 1,
+		borderColor: '#666',
+	},
 });
