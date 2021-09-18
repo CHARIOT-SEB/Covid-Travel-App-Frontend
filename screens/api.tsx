2e4dea176b1f73518c33847c8df70aade88f5a81
Mahamud Arteh <artehrush@gmail.com>
2021-09-18 14:22:21 +0100

    Info state intialised & fetchCountry working

--- screens/api.tsx
+++ screens/api.tsx
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 const coromerApi = axios.create({
-  baseURL: 'https://covid-travel-app-21.herokuapp.com/api'
+	baseURL: 'https://covid-travel-app-21.herokuapp.com/api',
 });
 
 export const getCountries = async () => {
-  const { data } = await coromerApi.get('/countries');
-  return data.countries;
+	const { data } = await coromerApi.get('/countries');
+	return data.countries;
+};
+
+export const getCountry = async (countryName: string) => {
+	const { data } = await coromerApi.get(`/countries/${countryName}`);
+	return data;
 };
