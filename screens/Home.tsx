553a13b9df83b85422bd5a3760d6884b18c15e32
Mahamud Arteh <artehrush@gmail.com>
2021-09-20 22:24:18 +0100

    Home page styled (draft)

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,7 @@
 // react imports
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 import Logo from '../components/Logo';
 // file imports
 import CountryPicker from '../navigation/CountryPicker';
@@ -10,10 +11,14 @@ const Home = (props: any) => {
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text>Coroamer</Text>
-			<View>
+			<View style={styles.content}>
 				<Logo />
-				<Button title='My Trips' onPress={() => nav.navigate('Trips')} />
+				<Text style={styles.text}>
+					Up-to-date travel information for 31 countries in Europe
+				</Text>
+				<TouchableHighlight onPress={() => nav.navigate('Trips')}>
+					<Text style={styles.button}>My Trips</Text>
+				</TouchableHighlight>
 				<CountryPicker nav={nav} />
 			</View>
 		</SafeAreaView>
@@ -22,10 +27,30 @@ const Home = (props: any) => {
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: 'rgb(153, 153, 230)',
-		flex: 1,
+		display: 'flex',
+		textAlign: 'center',
 		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: 'rgb(153, 153, 230)',
+	},
+	content: {
+		display: 'flex',
+		alignItems: 'center',
+	},
+	text: {
+		marginTop: 100,
+		marginBottom: 10,
+		fontSize: 15,
+	},
+	button: {
+		width: 125,
+		paddingTop: 20,
+		paddingBottom: 20,
+		color: '#fff',
+		textAlign: 'center',
+		backgroundColor: 'rgb(153, 153, 230)',
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: '#fff',
 	},
 });
 
