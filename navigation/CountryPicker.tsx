a25e6bc27f40bfc812d862d9a60bfc85699a829f
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-21 14:42:45 +0100

    changing font import

--- navigation/CountryPicker.tsx
+++ navigation/CountryPicker.tsx
@@ -9,54 +9,54 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { getCountries } from '../screens/api';
 import { dataStore } from '../providers/Data';
-import { useFonts, Oxygen_400Regular } from '@expo-google-fonts/dev';
+import { useFonts, Oxygen_400Regular } from '@expo-google-fonts/oxygen';
 
 function CountryPicker(props: any) {
-	// destructuring each state from the dataStore to be used
-	const { countryName, setCountryName } = useContext(dataStore);
-	const { countries, setCountries } = useContext(dataStore);
+  // destructuring each state from the dataStore to be used
+  const { countryName, setCountryName } = useContext(dataStore);
+  const { countries, setCountries } = useContext(dataStore);
 
-	const nav: any = props.nav;
+  const nav: any = props.nav;
 
-	//   set the countries from api
-	useEffect(() => {
-		getCountries().then((countries: any) => {
-			setCountries(countries);
-		});
-	}, []);
+  //   set the countries from api
+  useEffect(() => {
+    getCountries().then((countries: any) => {
+      setCountries(countries);
+    });
+  }, []);
 
-	return (
-		<View style={styles.screen}>
-			<Text style={styles.text}>Got a destination in mind? </Text>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>Got a destination in mind? </Text>
 
-			<Text>Choose here...</Text>
-			<Picker
-				selectedValue={countryName}
-				onValueChange={(value, index) => {
-					setCountryName(value);
-				}}
-				mode='dropdown' // Android only
-				style={styles.picker}
-			>
-				<Picker.Item label='Please choose a country' value='Unknown' />
-				{countries.map(country => {
-					return <Picker.Item key={country} label={country} value={country} />;
-				})}
-			</Picker>
-			<Pressable style={styles.button} onPress={() => nav.navigate('Country')}>
-				<Text
-					style={{
-						fontFamily: 'Oxygen_400Regular',
-						margin: 20,
-						fontSize: 20,
-						position: 'relative',
-					}}
-				>
-					Go
-				</Text>
-			</Pressable>
-		</View>
-	);
+      <Text>Choose here...</Text>
+      <Picker
+        selectedValue={countryName}
+        onValueChange={(value, index) => {
+          setCountryName(value);
+        }}
+        mode='dropdown' // Android only
+        style={styles.picker}
+      >
+        <Picker.Item label='Please choose a country' value='Unknown' />
+        {countries.map((country) => {
+          return <Picker.Item key={country} label={country} value={country} />;
+        })}
+      </Picker>
+      <Pressable style={styles.button} onPress={() => nav.navigate('Country')}>
+        <Text
+          style={{
+            fontFamily: 'Oxygen_400Regular',
+            margin: 20,
+            fontSize: 20,
+            position: 'relative'
+          }}
+        >
+          Go
+        </Text>
+      </Pressable>
+    </View>
+  );
 }
 
 export default CountryPicker;
@@ -65,27 +65,27 @@ const GREY = '#9E9E9E';
 
 // Some default styles
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	text: {
-		fontFamily: 'Oxygen_400Regular',
-		fontSize: 18,
-		margin: 40,
-		textAlign: 'center',
-	},
-	picker: {
-		marginVertical: 30,
-		width: 300,
-		padding: 10,
-	},
-	button: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#1D7253',
-		borderRadius: 10,
-		position: 'relative',
-	},
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontFamily: 'Oxygen_400Regular',
+    fontSize: 18,
+    margin: 40,
+    textAlign: 'center'
+  },
+  picker: {
+    marginVertical: 30,
+    width: 300,
+    padding: 10
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1D7253',
+    borderRadius: 10,
+    position: 'relative'
+  }
 });
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -5,79 +5,79 @@ import Logo from '../components/Logo';
 // file imports
 import CountryPicker from '../navigation/CountryPicker';
 // google fonts
-import { useFonts, Oxygen_400Regular } from '@expo-google-fonts/dev';
+import { useFonts, Oxygen_400Regular } from '@expo-google-fonts/oxygen';
 // loading base
 import { Spinner } from 'native-base';
 
 const Home = (props: any) => {
-	const nav = props.navigation;
+  const nav = props.navigation;
 
-	let [fontsLoaded] = useFonts({
-		Oxygen_400Regular,
-	});
+  let [fontsLoaded] = useFonts({
+    Oxygen_400Regular
+  });
 
-	if (!fontsLoaded) {
-		return (
-			<View style={{ flex: 1, justifyContent: 'center' }}>
-				<Spinner />
-			</View>
-		);
-	}
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={styles.content}>
-				<Logo />
-				<Text
-					style={{
-						flex: 0.15,
-						fontFamily: 'Oxygen_400Regular',
-						fontSize: 18,
-						margin: 40,
-						textAlign: 'center',
-					}}
-				>
-					Up-to-date travel information for 31 countries in Europe
-				</Text>
-				<Pressable style={styles.button} onPress={() => nav.navigate('Trips')}>
-					<Text
-						style={{
-							fontFamily: 'Oxygen_400Regular',
-							fontSize: 20,
-							margin: 10,
-							position: 'relative',
-						}}
-					>
-						My Trips
-					</Text>
-				</Pressable>
-				<CountryPicker nav={nav} />
-			</View>
-		</SafeAreaView>
-	);
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Spinner />
+      </View>
+    );
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Logo />
+        <Text
+          style={{
+            flex: 0.15,
+            fontFamily: 'Oxygen_400Regular',
+            fontSize: 18,
+            margin: 40,
+            textAlign: 'center'
+          }}
+        >
+          Up-to-date travel information for 31 countries in Europe
+        </Text>
+        <Pressable style={styles.button} onPress={() => nav.navigate('Trips')}>
+          <Text
+            style={{
+              fontFamily: 'Oxygen_400Regular',
+              fontSize: 20,
+              margin: 10,
+              position: 'relative'
+            }}
+          >
+            My Trips
+          </Text>
+        </Pressable>
+        <CountryPicker nav={nav} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		display: 'flex',
-		textAlign: 'center',
-		alignItems: 'center',
-		backgroundColor: '#DCEFF9',
-	},
-	content: {
-		display: 'flex',
-		alignItems: 'center',
-	},
-	text: {
-		flex: 0.3,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	button: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#1D7253',
-		borderRadius: 10,
-	},
+  container: {
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCEFF9'
+  },
+  content: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  text: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1D7253',
+    borderRadius: 10
+  }
 });
 
 export default Home;
