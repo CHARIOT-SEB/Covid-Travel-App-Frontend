6d98027401002c7595ee9ffb6d98c8cf0aeeb211
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-21 18:18:12 +0100

    adding loggedin/ isloggedIn

--- providers/Data.tsx
+++ providers/Data.tsx
@@ -4,24 +4,51 @@ import React, { createContext, useState } from 'react';
 export const dataStore = createContext({});
 
 const dataProvider = ({ children }) => {
-
-
-	// all states that need passing should sit here
-  let countryName, setCountryName, countries, setCountries, isLoading, setIsLoading, countryInfo, setCountryInfo, user, setUser, loginInfo, setLoginInfo: any
-	[countryName, setCountryName] = useState('');
-	[countries, setCountries] = useState([]);
-	[isLoading, setIsLoading] = useState(false);
-	[countryInfo, setCountryInfo] = useState({});
+  // all states that need passing should sit here
+  let countryName,
+    setCountryName,
+    countries,
+    setCountries,
+    isLoading,
+    setIsLoading,
+    countryInfo,
+    setCountryInfo,
+    user,
+    setUser,
+    loginInfo,
+    setLoginInfo,
+    isLoggedIn,
+    setIsLoggedIn: any;
+  [countryName, setCountryName] = useState('');
+  [countries, setCountries] = useState([]);
+  [isLoading, setIsLoading] = useState(false);
+  [countryInfo, setCountryInfo] = useState({});
   [user, setUser] = useState({});
   [loginInfo, setLoginInfo] = useState({});
-
-	// pass everything into 'value', so .Provider can provide everywhere in App
-	return (
-		<dataStore.Provider
-			value={{ countries, setCountries, countryName, setCountryName, countryInfo, setCountryInfo, isLoading, setIsLoading, user, setUser, loginInfo, setLoginInfo }}>
-			{children}
-		</dataStore.Provider>
-	);
+  [isLoggedIn, setIsLoggedIn] = useState(true);
+  // pass everything into 'value', so .Provider can provide everywhere in App
+  return (
+    <dataStore.Provider
+      value={{
+        countries,
+        setCountries,
+        countryName,
+        setCountryName,
+        countryInfo,
+        setCountryInfo,
+        isLoading,
+        setIsLoading,
+        user,
+        setUser,
+        loginInfo,
+        setLoginInfo,
+        isLoggedIn,
+        setIsLoggedIn
+      }}
+    >
+      {children}
+    </dataStore.Provider>
+  );
 };
 
 export default dataProvider;
