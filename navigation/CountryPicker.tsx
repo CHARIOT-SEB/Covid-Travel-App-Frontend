a24cd28f585a606163129e0b6f51b6bca46953ca
Rob Crowther <robcrowther@hotmail.com>
2021-09-21 08:25:36 +0100

    fix the country picker to pass country to Individule country page

--- navigation/CountryPicker.tsx
+++ navigation/CountryPicker.tsx
@@ -4,49 +4,49 @@ import 'react-native-gesture-handler';
 // react imports
 import React, { useEffect, useState } from 'react';
 import {
-	View,
-	Text,
-	StyleSheet,
-	Button,
-	TouchableHighlight,
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableHighlight
 } from 'react-native';
 // file imports
 import { Picker } from '@react-native-picker/picker';
 import { getCountries } from '../screens/api';
 
 function CountryPicker(props: any) {
-	const nav: any = props.nav;
-	const [country, setCountry] = useState('');
-	const [countries, setCountries] = useState([]);
+  const nav: any = props.nav;
+  const [country, setCountry] = useState('');
+  const [countries, setCountries] = useState([]);
 
-	//   set the countries from api
-	useEffect(() => {
-		getCountries().then((countries: any) => {
-			setCountries(countries);
-		});
-	}, []);
+  //   set the countries from api
+  useEffect(() => {
+    getCountries().then((countries: any) => {
+      setCountries(countries);
+    });
+  }, []);
 
-	return (
-		<View style={styles.screen}>
-			<Text style={styles.text}>Got a destination in mind? </Text>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>Got a destination in mind? </Text>
 
-			<Text>Choose here...</Text>
-			<Picker
-				selectedValue={country}
-				onValueChange={(value, index) => setCountry(value)}
-				mode='dropdown' // Android only
-				style={styles.picker}
-			>
-				<Picker.Item label='Please choose a country' value='Unknown' />
-				{countries.map(country => {
-					return <Picker.Item key={country} label={country} value={country} />;
-				})}
-			</Picker>
-			<TouchableHighlight onPress={() => nav.navigate('Country')}>
-				<Text style={styles.button}>Go</Text>
-			</TouchableHighlight>
-		</View>
-	);
+      <Text>Choose here...</Text>
+      <Picker
+        selectedValue={country}
+        onValueChange={(value, index) => setCountry(value)}
+        mode="dropdown" // Android only
+        style={styles.picker}
+      >
+        <Picker.Item label="Please choose a country" value="Unknown" />
+        {countries.map((country) => {
+          return <Picker.Item key={country} label={country} value={country} />;
+        })}
+      </Picker>
+      <TouchableHighlight onPress={() => nav.navigate('Country', country)}>
+        <Text style={styles.button}>Go</Text>
+      </TouchableHighlight>
+    </View>
+  );
 }
 
 export default CountryPicker;
@@ -55,28 +55,28 @@ const GREY = '#9E9E9E';
 
 // Some default styles
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	text: {
-		fontSize: 24,
-	},
-	picker: {
-		marginVertical: 30,
-		width: 300,
-		padding: 10,
-	},
-	button: {
-		width: 125,
-		paddingTop: 20,
-		paddingBottom: 20,
-		color: '#fff',
-		textAlign: 'center',
-		backgroundColor: 'rgb(80, 186, 9)',
-		borderRadius: 10,
-		borderWidth: 1,
-		borderColor: '#fff',
-	},
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24
+  },
+  picker: {
+    marginVertical: 30,
+    width: 300,
+    padding: 10
+  },
+  button: {
+    width: 125,
+    paddingTop: 20,
+    paddingBottom: 20,
+    color: '#fff',
+    textAlign: 'center',
+    backgroundColor: 'rgb(80, 186, 9)',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  }
 });
