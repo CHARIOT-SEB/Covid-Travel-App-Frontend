cbfcc517a1d4ffd629d066dbbffb3b0f71377e7a
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-21 12:39:28 +0100

    added state for trips

--- providers/Data.tsx
+++ providers/Data.tsx
@@ -9,21 +9,38 @@ import React, { createContext, useState } from 'react';
 export const dataStore = createContext({});
 
 const dataProvider = ({ children }) => {
-	// all states that need passing should sit here
-	const [country, setCountry] = useState('');
-	const [boo, setBoo] = useState(false);
-	const [countries, setCountries] = useState([]);
-	
+  // all states that need passing should sit here
+  const [country, setCountry] = useState('');
+  const [boo, setBoo] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [loginInfo, setLoginInfo] = useState({});
 
-	console.log(country, '<--------- in provider');
-	console.log(boo, '<--------- in provider');
+  console.log(country, '<--------- in provider');
+  console.log(boo, '<--------- in provider');
 
-	// pass everything into 'value', so .Provider can provide everywhere in App
-	return (
-		<dataStore.Provider value={{ country, setCountry, boo, setBoo, countries, setCountries}}>
-			{children}
-		</dataStore.Provider>
-	);
+  // pass everything into 'value', so .Provider can provide everywhere in App
+  return (
+    <dataStore.Provider
+      value={{
+        country,
+        setCountry,
+        boo,
+        setBoo,
+        countries,
+        setCountries,
+        user,
+        setUser,
+        isLoading,
+        setIsLoading,
+        loginInfo,
+        setLoginInfo
+      }}
+    >
+      {children}
+    </dataStore.Provider>
+  );
 };
 
 export default dataProvider;
