7793297ce60ac26737afd93377af3766be9b24c2
Rob Crowther <robcrowther@hotmail.com>
2021-09-22 09:46:05 +0100

    country infor components

--- screens/IndividualCountry.tsx
+++ screens/IndividualCountry.tsx
@@ -1,154 +1,171 @@
-import React, { useEffect, useContext} from 'react';
-import { View, Text, StyleSheet, ScrollView, } from 'react-native';
+import React, { useEffect, useContext } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import MapView from 'react-native-maps';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { getCountry } from '../screens/api';
 import Logo from '../components/Logo';
 import IsVaccinated from '../components/IsVaccinated';
 import IsntVaccinated from '../components/IsntVaccinated';
+import Restrictions from '../components/Restrictions';
+import Returning from '../components/Returning';
 import { Spinner } from 'native-base';
 import { dataStore } from '../providers/Data';
 
 const IndividualCountry = () => {
-	const { countryName, countryInfo, setCountryInfo, isLoading, setIsLoading } = useContext(dataStore);
+  const { countryName, countryInfo, setCountryInfo, isLoading, setIsLoading } =
+    useContext(dataStore);
 
-	useEffect(() => {
-		setIsLoading(true);
-		getCountry(countryName)
-			.then((country: any) => {
-				setCountryInfo(country);
-			})
-			.then(() => {
-				setIsLoading(false);
-			});
-	}, [countryName]);
+  useEffect(() => {
+    setIsLoading(true);
+    getCountry(countryName)
+      .then((country: any) => {
+        setCountryInfo(country);
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
+  }, [countryName]);
 
-	if (!countryInfo.country) return null;
+  if (!countryInfo.country) return null;
 
-	if (isLoading) {
-		return (
-			<View style={{ flex: 1, justifyContent: 'center' }}>
-				<Spinner />
-			</View>
-		);
-	}
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Spinner />
+      </View>
+    );
+  }
 
-	return (
-		<SafeAreaView>
-            <ScrollView>
-                <View style={styles.container}>
-                    <Logo />
-                    <View style={styles.trafficLight}>
-                        <Text style={styles.name}>{countryInfo.country}</Text>
-                    </View>
-                    <Text>{countryInfo.colorList}</Text>
-                    <MapView
-                        style={styles.map}
-                        showsUserLocation={true}
-                        region={countryInfo.geoLocation}
-                        // user location will be available to see, if location services are enabled
-                    />
-                    <IsVaccinated countryInfo={countryInfo} />
-                    <IsntVaccinated countryInfo={countryInfo} />
-                </View>
-            </ScrollView>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Logo />
+          <View style={styles.trafficLight}>
+            <Text style={styles.name}>{countryInfo.country}</Text>
+          </View>
+          <Text>{countryInfo.colorList}</Text>
+          <MapView
+            style={styles.map}
+            showsUserLocation={true}
+            region={countryInfo.geoLocation}
+            // user location will be available to see, if location services are enabled
+          />
+          <View>
+            <IsVaccinated />
+          </View>
+          <View>
+            <IsntVaccinated />
+          </View>
+          <View>
+            <Restrictions />
+          </View>
+          <View>
+            <Returning />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-	},
-	map: {
-		alignSelf: 'center',
-		width: 370,
-		height: 500,
+  container: {
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCEFF9'
+  },
+  map: {
+    alignSelf: 'center',
+    width: 370,
+    height: 500,
 
-		alignItems: 'center',
-		borderWidth: 3,
-		borderColor: '#ddd',
-		borderRadius: 6,
-	},
-	trafficLight: {
-		padding: 20,
-		backgroundColor: '#4d94ff',
-		borderRadius: 15,
-	},
-	name: {
-		fontSize: 30,
-	},
-	logo: {
-		flex: 1,
-		height: 20,
-		padding: 20,
-		margin: 10,
-		borderRadius: 80,
-		alignItems: 'stretch',
-		backgroundColor: '#5f9ea0',
-	},
-	countryTitle: {
-		height: 5,
-		margin: 20,
-		padding: 20,
-		width: 70,
-		backgroundColor: '#ff7f50',
-		borderRadius: 20,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	heatMapContainer: {
-		flex: 3,
-		flexDirection: 'row',
-		margin: 25,
-		height: 20,
-		alignItems: 'stretch',
-		alignSelf: 'stretch',
-		padding: 50,
-		backgroundColor: '#fff8dc',
-		justifyContent: 'center',
-	},
-	heatMap: {
-		flexDirection: 'row',
-		margin: 10,
-		color: '#000000',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	countryInfoContainer: {
-		backgroundColor: '#fff8dc',
-		flex: 4,
-		flexDirection: 'column',
-		alignSelf: 'stretch',
-		margin: 25,
-		height: 20,
-		padding: 50,
-	},
-	info: {
-		margin: 2,
-		alignSelf: 'center',
-	},
-	infoContainer: {
-		margin: 20,
-		padding: 20,
-		backgroundColor: '#4d94ff',
-		borderRadius: 15,
-	},
-	text: {
-		textAlign: 'center',
-		margin: 5,
-		fontSize: 15,
-	},
-	loading: {
-		position: 'absolute',
-		left: 0,
-		right: 0,
-		top: 0,
-		bottom: 0,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+    alignItems: 'center',
+    borderWidth: 3,
+    borderColor: '#ddd',
+    borderRadius: 6
+  },
+  trafficLight: {
+    padding: 20,
+    backgroundColor: '#5c98c0',
+    borderRadius: 10
+  },
+  name: {
+    fontSize: 30,
+    fontFamily: 'Oxygen_700Bold',
+    color: 'black'
+  },
+  logo: {
+    flex: 1,
+    height: 20,
+    padding: 20,
+    margin: 10,
+    borderRadius: 80,
+    alignItems: 'stretch',
+    backgroundColor: '#5f9ea0'
+  },
+  countryTitle: {
+    height: 5,
+    margin: 20,
+    padding: 20,
+    width: 70,
+    backgroundColor: '#1D7253',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  heatMapContainer: {
+    flex: 3,
+    flexDirection: 'row',
+    margin: 25,
+    height: 20,
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
+    padding: 50,
+    backgroundColor: '#fff8dc',
+    justifyContent: 'center'
+  },
+  heatMap: {
+    flexDirection: 'row',
+    margin: 10,
+    color: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  countryInfoContainer: {
+    backgroundColor: '#fff8dc',
+    flex: 4,
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+    margin: 25,
+    height: 20,
+    padding: 50
+  },
+  info: {
+    margin: 2,
+    alignSelf: 'center'
+  },
+  infoContainer: {
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#4d94ff',
+    borderRadius: 15
+  },
+  text: {
+    textAlign: 'center',
+    margin: 5,
+    fontSize: 15
+  },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
 
 export default IndividualCountry;
