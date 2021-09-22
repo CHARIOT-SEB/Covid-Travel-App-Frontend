457500893352860a35bdd69c8f0294917a4f0eab
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-22 14:42:48 +0100

    merging forms together

--- components/formComponent.tsx
+++ components/formComponent.tsx
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { Container } from 'native-base';
@@ -7,135 +13,141 @@ import { dataStore } from '../providers/Data';
 import { postNewAccount } from '../screens/api';
 
 const userSchema = yup.object({
-	name: yup.string().required(),
-	email: yup.string().required().email(),
-	password: yup.string().min(4).max(10).required(),
-	// can do a .matches(takes a regex for password rules)
-	// can do a .test(takes 3 args, name of func, message to user, function to test input)
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().min(4).max(10).required()
+  // can do a .matches(takes a regex for password rules)
+  // can do a .test(takes 3 args, name of func, message to user, function to test input)
 });
 const formComponent = (props: any) => {
-	const nav = props.navigation;
+  const nav = props.navigation;
 
-	const { setSignUp, setUser, user, setLoginInfo, setIsLoggedIn } = useContext(dataStore);
+  const { setSignUp, setUser, user, setLoginInfo, setIsLoggedIn } =
+    useContext(dataStore);
 
-	useEffect(() => {
-		postNewAccount(user)
-			.then((data) => {
-				setLoginInfo(data);
-				setIsLoggedIn(true);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, [user]);
+  useEffect(() => {
+    postNewAccount(user)
+      .then((data) => {
+        setLoginInfo(data);
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [user]);
 
-	return (
-		<Container>
-			<View style={styles.formContainer}>
-				<Text>Join now!</Text>
-				<Formik
-					initialValues={{ name: '', email: '', password: '' }}
-					validationSchema={userSchema}
-					onSubmit={(values, actions) => {
-						setUser(values);
-						console.log(user);
-						actions.resetForm();
-						setSignUp(false);
-					}}
-				>
-					{(props) => (
-						<View>
-							<Text>Name</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Name'
-								onChangeText={props.handleChange('name')}
-								value={props.values.name}
-								onBlur={props.handleBlur('name')}
-							/>
-							<Text style={styles.errorText}> {props.touched.name && props.errors.name} </Text>
+  return (
+    <Container>
+      <View style={styles.formContainer}>
+        <Text>Join now!</Text>
+        <Formik
+          initialValues={{ name: '', email: '', password: '' }}
+          validationSchema={userSchema}
+          onSubmit={(values, actions) => {
+            setUser(values);
+            actions.resetForm();
+            setSignUp(false);
+          }}
+        >
+          {(props) => (
+            <View>
+              <Text>Name</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Name'
+                onChangeText={props.handleChange('name')}
+                value={props.values.name}
+                onBlur={props.handleBlur('name')}
+              />
+              <Text style={styles.errorText}>
+                {' '}
+                {props.touched.name && props.errors.name}{' '}
+              </Text>
 
-							<Text>Email</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Email'
-								onChangeText={props.handleChange('email')}
-								value={props.values.email}
-								onBlur={props.handleBlur('email')}
-							/>
-							<Text style={styles.errorText}> {props.touched.email && props.errors.email} </Text>
+              <Text>Email</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Email'
+                onChangeText={props.handleChange('email')}
+                value={props.values.email}
+                onBlur={props.handleBlur('email')}
+              />
+              <Text style={styles.errorText}>
+                {' '}
+                {props.touched.email && props.errors.email}{' '}
+              </Text>
 
-							<Text>Password</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Password'
-								onChangeText={props.handleChange('password')}
-								value={props.values.password}
-								onBlur={props.handleBlur('password')}
-								secureTextEntry
-							/>
-							<Text style={styles.errorText}>
-								{props.touched.password && props.errors.password}
-							</Text>
+              <Text>Password</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Password'
+                onChangeText={props.handleChange('password')}
+                value={props.values.password}
+                onBlur={props.handleBlur('password')}
+                secureTextEntry
+              />
+              <Text style={styles.errorText}>
+                {props.touched.password && props.errors.password}
+              </Text>
 
-							<TouchableOpacity
-								style={styles.btn}
-								onPress={() => {
-									props.handleSubmit();
-								}}
-							>
-								<Text style={styles.btnText}>Sign Up</Text>
-							</TouchableOpacity>
-						</View>
-					)}
-				</Formik>
-				<View>
-					<TouchableOpacity style={styles.btn} onPress={() => setSignUp(false)}>
-						<Text style={styles.btnText}> Back </Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		</Container>
-	);
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => {
+                  props.handleSubmit();
+                }}
+              >
+                <Text style={styles.btnText}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+        <View>
+          <TouchableOpacity style={styles.btn} onPress={() => setSignUp(false)}>
+            <Text style={styles.btnText}> Back </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
-	formContainer: {
-		alignItems: 'center',
-		// borderWidth: 3,
-		// borderColor: 'black',
-		// borderRadius: 6,
-	},
-	input: {
-		width: 300,
-		height: 40,
-		backgroundColor: 'white',
-		borderWidth: 2,
-		borderColor: '#ddd',
-		fontSize: 18,
-		borderRadius: 10,
-	},
-	errorText: {
-		color: 'crimson',
-		fontWeight: 'bold',
-		marginBottom: 3,
-		marginTop: 3,
-		textAlign: 'center',
-	},
-	btn: {
-		borderRadius: 8,
-		marginVertical: 8,
-		paddingVertical: 14,
-		paddingHorizontal: 10,
-		backgroundColor: '#5c98c0',
-	},
-	btnText: {
-		color: 'white',
-		fontWeight: 'bold',
-		textTransform: 'uppercase',
-		fontSize: 16,
-		textAlign: 'center',
-	},
+  formContainer: {
+    alignItems: 'center'
+    // borderWidth: 3,
+    // borderColor: 'black',
+    // borderRadius: 6,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: '#ddd',
+    fontSize: 18,
+    borderRadius: 10
+  },
+  errorText: {
+    color: 'crimson',
+    fontWeight: 'bold',
+    marginBottom: 3,
+    marginTop: 3,
+    textAlign: 'center'
+  },
+  btn: {
+    borderRadius: 8,
+    marginVertical: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    backgroundColor: '#5c98c0'
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    textAlign: 'center'
+  }
 });
 
 export default formComponent;
--- screens/SignUpForm.tsx
+++ screens/SignUpForm.tsx
@@ -1,12 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import {
-	Text,
-	View,
-	StyleSheet,
-	SafeAreaView,
-	TouchableOpacity,
-	TextInput,
-	Button,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 import Logo from '../components/Logo';
 import { dataStore } from '../providers/Data';
@@ -16,160 +15,160 @@ import { postNewAccount } from '../screens/api';
 import { useNavigation } from '@react-navigation/native';
 
 const userSchema = yup.object({
-	name: yup.string().required(),
-	email: yup.string().required().email(),
-	password: yup.string().min(4).max(10).required(),
-	// can do a .matches(takes a regex for password rules)
-	// can do a .test(takes 3 args, name of func, message to user, function to test input)
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().min(4).max(10).required()
 });
 
 const SignUpForm = (props: any) => {
-	const nav = useNavigation();
+  if (isLoggedIn) return null;
 
-	const { isLoggedIn, setSignUp, setUser, user, setLoginInfo, setIsLoggedIn } =
-		useContext(dataStore);
+  const nav = useNavigation();
 
-	useEffect(() => {
-		postNewAccount(user)
-			.then((data) => {
-				setLoginInfo(data);
-				setIsLoggedIn(true);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, [user]);
+  const { isLoggedIn, setSignUp, setUser, user, setIsLoggedIn } =
+    useContext(dataStore);
 
-	if (isLoggedIn) return null;
+  const addAccount = (user: any) => {
+    postNewAccount(user)
+      .then((data) => {
+        setUser(data);
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={styles.logo}>
-				<Logo />
-			</View>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logo}>
+        <Logo />
+      </View>
 
-			{/* <FormComponent /> */}
-			<View style={styles.formContainer}>
-				<Text>Join now!</Text>
-				<Formik
-					initialValues={{ name: '', email: '', password: '' }}
-					validationSchema={userSchema}
-					onSubmit={(values, actions) => {
-						setUser(values);
-						actions.resetForm();
-						setSignUp(false);
-					}}
-				>
-					{(props) => (
-						<View>
-							<Text>Name</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Name'
-								onChangeText={props.handleChange('name')}
-								value={props.values.name}
-								onBlur={props.handleBlur('name')}
-							/>
-							<Text style={styles.errorText}> {props.touched.name && props.errors.name} </Text>
+      {/* <FormComponent /> */}
+      <View style={styles.formContainer}>
+        <Text>Join now!</Text>
+        <Formik
+          initialValues={{ name: '', email: '', password: '' }}
+          validationSchema={userSchema}
+          onSubmit={(values, actions) => {
+            addAccount(values);
+            actions.resetForm();
+            setSignUp(false);
+          }}
+        >
+          {(props) => (
+            <View>
+              <Text>Name</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Name'
+                onChangeText={props.handleChange('name')}
+                value={props.values.name}
+                onBlur={props.handleBlur('name')}
+              />
+              <Text style={styles.errorText}>
+                {' '}
+                {props.touched.name && props.errors.name}{' '}
+              </Text>
 
-							<Text>Email</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Email'
-								onChangeText={props.handleChange('email')}
-								value={props.values.email}
-								onBlur={props.handleBlur('email')}
-							/>
-							<Text style={styles.errorText}> {props.touched.email && props.errors.email} </Text>
+              <Text>Email</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Email'
+                onChangeText={props.handleChange('email')}
+                value={props.values.email}
+                onBlur={props.handleBlur('email')}
+              />
+              <Text style={styles.errorText}>
+                {' '}
+                {props.touched.email && props.errors.email}{' '}
+              </Text>
 
-							<Text>Password</Text>
-							<TextInput
-								style={styles.input}
-								placeholder='Password'
-								onChangeText={props.handleChange('password')}
-								value={props.values.password}
-								onBlur={props.handleBlur('password')}
-								secureTextEntry
-							/>
-							<Text style={styles.errorText}>
-								{props.touched.password && props.errors.password}
-							</Text>
+              <Text>Password</Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Password'
+                onChangeText={props.handleChange('password')}
+                value={props.values.password}
+                onBlur={props.handleBlur('password')}
+                secureTextEntry
+              />
+              <Text style={styles.errorText}>
+                {props.touched.password && props.errors.password}
+              </Text>
 
-							<TouchableOpacity
-								style={styles.btn}
-								onPress={() => {
-									props.handleSubmit();
-								}}
-							>
-								<Text style={styles.btnText}>Sign Up</Text>
-							</TouchableOpacity>
-						</View>
-					)}
-				</Formik>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => {
+                  props.handleSubmit();
+                  // setIsLoggedIn(true);
+                  nav.navigate('Home');
+                }}
+              >
+                <Text style={styles.btnText}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
 
-				<View>
-					<TouchableOpacity style={styles.btn} onPress={() => setSignUp(false)}>
-						<Text style={styles.btnText}> Back </Text>
-					</TouchableOpacity>
-				</View>
-
-				<View>
-					<TouchableOpacity style={styles.btn} onPress={() => {nav.navigate('Home'); console.log('clicking home button');}}>
-						<Text style={styles.btnText}> Home </Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		</SafeAreaView>
-	);
+        <View>
+          <TouchableOpacity style={styles.btn} onPress={() => setSignUp(false)}>
+            <Text style={styles.btnText}> Back </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 2,
-		backgroundColor: '#DCEFF9',
-	},
-	formContainer: {
-		alignItems: 'center',
-		// borderWidth: 3,
-		// borderColor: 'black',
-		// borderRadius: 6,
-	},
-	logo: {
-		alignItems: 'center',
-		marginBottom: 100,
-		padding: 20,
-		backgroundColor: '#DCEFF9',
-	},
-	input: {
-		width: 300,
-		height: 40,
-		backgroundColor: 'white',
-		borderWidth: 2,
-		borderColor: '#ddd',
-		fontSize: 18,
-		borderRadius: 10,
-	},
-	errorText: {
-		color: 'crimson',
-		fontWeight: 'bold',
-		marginBottom: 3,
-		marginTop: 3,
-		textAlign: 'center',
-	},
-	btn: {
-		borderRadius: 8,
-		marginVertical: 8,
-		paddingVertical: 14,
-		paddingHorizontal: 10,
-		backgroundColor: '#5c98c0',
-	},
-	btnText: {
-		color: 'white',
-		fontWeight: 'bold',
-		textTransform: 'uppercase',
-		fontSize: 16,
-		textAlign: 'center',
-	},
+  container: {
+    flex: 2,
+    backgroundColor: '#DCEFF9'
+  },
+  formContainer: {
+    alignItems: 'center'
+    // borderWidth: 3,
+    // borderColor: 'black',
+    // borderRadius: 6,
+  },
+  logo: {
+    alignItems: 'center',
+    marginBottom: 100,
+    padding: 20,
+    backgroundColor: '#DCEFF9'
+  },
+  input: {
+    width: 300,
+    height: 40,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: '#ddd',
+    fontSize: 18,
+    borderRadius: 10
+  },
+  errorText: {
+    color: 'crimson',
+    fontWeight: 'bold',
+    marginBottom: 3,
+    marginTop: 3,
+    textAlign: 'center'
+  },
+  btn: {
+    borderRadius: 8,
+    marginVertical: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    backgroundColor: '#5c98c0'
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    textAlign: 'center'
+  }
 });
 
 export default SignUpForm;
